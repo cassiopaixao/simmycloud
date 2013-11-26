ba0de5d8399ae7b71c15f3c2e43ed050c01d8763
Cassio Paixao <cassio.paixao@gmail.com>
2013-11-26 15:40:50 -0200

    Basic debugging in scheduling strategies

--- simmycloud/strategies/scheduling/scheduling_strategy.py
+++ simmycloud/strategies/scheduling/scheduling_strategy.py
@@ -8,6 +8,8 @@ class SchedulingStrategy(Strategy):
             res = func(self, *args, **kwargs)
             if res is None:
                 self._config.statistics.add_to_counter('vms_not_allocated')
+                self._config.getLogger(self).debug('VM {} was not allocated'.format(args[0].name))
+            self._config.getLogger(self).debug('VM {} was allocated to server {}'.format(args[0].name, res.name))
             return res
         return new_schedule_vm
 
