92c0f107bea86a65998f4a50a3a976716273ca89
Cassio Paixao <cassio.paixao@gmail.com>
2013-11-28 16:19:22 -0200

    FirstFit strategy refactored.

--- simmycloud/strategies/scheduling/first_fit.py
+++ simmycloud/strategies/scheduling/first_fit.py
@@ -5,19 +5,24 @@ class FirstFit(SchedulingStrategy):
 
     @SchedulingStrategy.schedule_vm_statistics
     def schedule_vm(self, vm):
-        servers = self._config.environment.online_servers()
+        server = self.get_first_fit(vm,
+                                    self._config.environment.online_servers())
+
+        if server is None:
+            server = self.get_first_fit(vm,
+                                        self._config.environment.offline_servers())
+            if server is not None:
+                self._config.environment.turn_on_server(server.name)
+
+        if server is not None:
+            self._config.environment.schedule_vm_at_server(vm, server.name)
+
+        return server
+
+
+    def get_first_fit(self, vm, servers):
         for server in servers:
             if server.cpu - server.cpu_alloc >= vm.cpu and \
                server.mem - server.mem_alloc >= vm.mem:
-                self._config.environment.schedule_vm_at_server(vm, server.name)
                 return server
-
-        offline_servers = self._config.environment.offline_servers()
-        for server in offline_servers:
-            if server.cpu - server.cpu_alloc >= vm.cpu and \
-               server.mem - server.mem_alloc >= vm.mem:
-                self._config.environment.turn_on_server(server.name)
-                self._config.environment.schedule_vm_at_server(vm, server.name)
-                return server
-
         return None
