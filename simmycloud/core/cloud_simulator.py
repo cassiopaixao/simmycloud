fb88d1b7a6c7b2ee1e43f9a9ca2bef84c563b09d
Cassio Paixao <cassio.paixao@gmail.com>
2014-01-05 21:13:23 -0200

    First changes to events processing

--- simmycloud/core/cloud_simulator.py
+++ simmycloud/core/cloud_simulator.py
@@ -25,42 +25,32 @@ class CloudSimulator:
 
     def _process_event(self, event):
         strategies = self._config.strategies
-        server = None
 
-        if event.type == EventType.SCHEDULE:
+        if event.type == EventType.SUBMIT:
             self._config.statistics.notify_event('submit_events')
-            server = strategies.scheduling.schedule_vm(event.vm)
+            strategies.scheduling.schedule_vm(event.vm)
 
-        elif event.type == EventType.UPDATE_RUNNING:
+        elif event.type == EventType.UPDATE:
             self._config.statistics.notify_event('update_events')
             self._config.environment.update_vm_demands(event.vm)
             server = self._config.environment.get_server_of_vm(event.vm.name)
             if server is not None:
+                # vm is allocated
                 strategies.migration.migrate_from_server_if_necessary(server)
-                strategies.powering_off.power_off_if_necessary(server)
+            else:
+                # vm is in vms_pool
+                # TODO verify if update_vm_demands updates even if vm
+                # is not allocated
+                pass
 
         elif event.type == EventType.FINISH:
             self._config.statistics.notify_event('finish_events')
             self._config.environment.free_vm_resources(event.vm)
-            server = self._config.environment.get_server_of_vm(event.vm.name)
-            if server is not None:
-                strategies.powering_off.power_off_if_necessary(server)
 
-        elif event.type in [EventType.SUBMIT, EventType.UPDATE_PENDING]:
+        elif event.type == EventType.NOTIFY:
+            # TODO implement this case.
             pass
 
         else:
             self._logger.error('Unknown event: %s'.format(event.dump()))
             raise Exception('Unknown event: %s'.format(event.dump()))
-
-        if server is not None and CloudUtils.is_overloaded(server):
-            self._logger.error('Server is overloaded: %s', server.dump())
-            for vm in server.vm_list():
-                self._logger.error('VM in server %s: %s', server.name, vm.dump())
-            raise Exception('Server is overloaded: %s', server.dump())
-
-
-class CloudUtils:
-    @staticmethod
-    def is_overloaded(server):
-        return (server.cpu_alloc > server.cpu or server.mem_alloc > server.mem)
