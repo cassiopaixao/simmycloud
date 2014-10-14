2ba81fa53340258bf4b7ed577e976a415295aa2d
Cassio Paixao <cassio.paixao@gmail.com>
2014-10-14 12:24:57 -0300

    update lib version

--- c-src/setup.py
+++ c-src/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup, Extension
 import glob
- 
+
 module = Extension('rbf', language = 'c', sources = ['rbf.c'] + glob.glob('routines/*c'))
- 
+
 setup (name = 'rbf',
-        version = '1.0',
+        version = '1.0.1',
         description = 'Utility package to model data using a radial basis function (rbf) ansatz.',
         maintainer = 'Daniel Cordeiro',
         maintainer_email = 'danielc@ime.usp.br',
